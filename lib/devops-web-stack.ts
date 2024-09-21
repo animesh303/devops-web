a3fd54012c8965d92195d12cbfddd7fc44c61675
Animesh Kumar Naskar <animesh.kumar.naskar@accenture.com>
2024-09-21 20:05:33 +0530

    Enhanced for multiple deplotyments

--- lib/devops-web-stack.ts
+++ lib/devops-web-stack.ts
@@ -5,7 +5,7 @@ import * as codepipeline_actions from "aws-cdk-lib/aws-codepipeline-actions";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import { Construct } from "constructs";
 import { Secret } from "aws-cdk-lib/aws-secretsmanager";
-import { SecretValue } from "aws-cdk-lib";
+import { Duration, SecretValue } from "aws-cdk-lib";
 import * as codebuild from "aws-cdk-lib/aws-codebuild";
 
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
@@ -16,7 +16,21 @@ export class WebDevOpsPipelineStack extends cdk.Stack {
     super(scope, id, props);
 
     const accountId = this.account;
-    const bucketName = `webapp-${accountId}-${cdk.Aws.REGION}`;
+
+    // const githubToken = Context.getValue(this, "githubToken");
+    const githubToken = this.node.tryGetContext("githubToken");
+    const githubOwner = this.node.tryGetContext("githubOwner");
+    const githubRepo = this.node.tryGetContext("githubRepo");
+    const githubBranch = this.node.tryGetContext("githubBranch");
+
+    // Create a Secrets Manager secret
+    const githubSecret = new Secret(this, `GithubSecret-${githubRepo}`, {
+      secretName: `GitHub-Token-${githubRepo}`,
+      description: `GitHub personal access token for ${githubRepo}`,
+      secretStringValue: SecretValue.plainText(githubToken),
+    });
+
+    const bucketName = `webapp-${githubRepo}-${accountId}-${cdk.Aws.REGION}`;
     // Create an S3 bucket for the website
     const websiteBucket = new s3.Bucket(this, "webappBucket", {
       bucketName: bucketName, // Replace with your desired bucket name
@@ -32,27 +46,22 @@ export class WebDevOpsPipelineStack extends cdk.Stack {
     });
 
     // Create the CodePipeline
-    const pipeline = new codepipeline.Pipeline(this, "webAppPipeline", {
-      pipelineName: "webAppPipeline",
-    });
+    // const pipeline = new codepipeline.Pipeline(this, "webAppPipeline", {
+    //   pipelineName: "webAppPipeline",
+    // });
 
-    // const githubToken = Context.getValue(this, "githubToken");
-    const githubToken = this.node.tryGetContext("githubToken");
-    const githubOwner = this.node.tryGetContext("githubOwner");
-    const githubRepo = this.node.tryGetContext("githubRepo");
-    const githubBranch = this.node.tryGetContext("githubBranch");
-
-    // Create a Secrets Manager secret
-    const githubSecret = new Secret(this, "GithubSecret", {
-      secretName: "GitHub-Token",
-      description: "GitHub personal access token",
-      secretStringValue: SecretValue.plainText(githubToken),
-    });
+    const pipeline = new codepipeline.Pipeline(
+      this,
+      `WebAppPipeline-${githubRepo}`,
+      {
+        pipelineName: `webAppPipeline-${githubRepo}`,
+      }
+    );
 
     // Add the source stage (GitHub repository)
     const sourceOutput = new codepipeline.Artifact();
     const sourceAction = new codepipeline_actions.GitHubSourceAction({
-      actionName: "WebAppSource",
+      actionName: `WebAppSource-${githubRepo}`,
       owner: githubOwner,
       repo: githubRepo,
       branch: githubBranch,
@@ -65,37 +74,42 @@ export class WebDevOpsPipelineStack extends cdk.Stack {
     });
 
     // Define the Build project
-    const buildProject = new codebuild.PipelineProject(this, "BuildProject", {
-      buildSpec: codebuild.BuildSpec.fromObject({
-        version: "0.2",
-        phases: {
-          install: {
-            commands: [
-              "npm install", // Install dependencies
+    const buildProject = new codebuild.PipelineProject(
+      this,
+      `BuildProject-${githubRepo}`,
+      {
+        projectName: `BuildProject-${githubRepo}`,
+        buildSpec: codebuild.BuildSpec.fromObject({
+          version: "0.2",
+          phases: {
+            install: {
+              commands: [
+                "npm install", // Install dependencies
+              ],
+            },
+            build: {
+              commands: [
+                "npm run build", // Build the project, assuming it outputs to the 'build' folder
+              ],
+            },
+          },
+          artifacts: {
+            "base-directory": "build", // Specify the build output directory
+            files: [
+              "**/*", // Include all files in the build directory
             ],
           },
-          build: {
-            commands: [
-              "npm run build", // Build the project, assuming it outputs to the 'build' folder
-            ],
-          },
+        }),
+        environment: {
+          buildImage: codebuild.LinuxBuildImage.STANDARD_5_0, // Choose appropriate build image
         },
-        artifacts: {
-          "base-directory": "build", // Specify the build output directory
-          files: [
-            "**/*", // Include all files in the build directory
-          ],
-        },
-      }),
-      environment: {
-        buildImage: codebuild.LinuxBuildImage.STANDARD_5_0, // Choose appropriate build image
-      },
-    });
+      }
+    );
 
     // Add the Build stage
     const buildOutput = new codepipeline.Artifact();
     const buildAction = new codepipeline_actions.CodeBuildAction({
-      actionName: "Build",
+      actionName: `Build-${githubRepo}`,
       project: buildProject,
       input: sourceOutput,
       outputs: [buildOutput], // Output artifact from build
@@ -118,6 +132,17 @@ export class WebDevOpsPipelineStack extends cdk.Stack {
       actions: [deployAction],
     });
 
+    // Create a custom cache policy
+    const customCachePolicy = new cloudfront.CachePolicy(
+      this,
+      "CustomCachePolicy",
+      {
+        defaultTtl: Duration.minutes(1), // Set the default TTL to 1 minutes
+        minTtl: Duration.seconds(1), // Minimum TTL of 1 second
+        maxTtl: Duration.minutes(2), // Maximum TTL of 2 minutes
+      }
+    );
+
     // Add CloudFront distribution
     const distribution = new cloudfront.Distribution(
       this,
@@ -127,6 +152,7 @@ export class WebDevOpsPipelineStack extends cdk.Stack {
           origin: new origins.S3StaticWebsiteOrigin(websiteBucket),
           viewerProtocolPolicy:
             cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
+          cachePolicy: customCachePolicy,
         },
         defaultRootObject: "index.html",
         errorResponses: [
